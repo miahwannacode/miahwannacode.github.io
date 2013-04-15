034a5162904469088ea677220d20c9ca2c321077
Jeremiah Abalos <abalos.jeremiah@gmail.com>
2013-04-15 22:55:49 +0800

    Synching assets and HTML edits

--- assets/js/miah.js
+++ assets/js/miah.js
@@ -18,3 +18,4 @@ $(document).ready(function(){
 			pagination: "div.carousel-indicators"
 		});
 	});
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
