332e58929f8e0acaafde3bc8a7d7caa08bb1e123
Jeremiah Abalos <abalos.jeremiah@gmail.com>
2013-04-16 17:13:16 +0800

    Added Google Analytics tracker to index.html

--- assets/js/miah.js
+++ assets/js/miah.js
@@ -1,20 +1,7 @@
-$(document).ready(function(){
-		$(".carousel-inner").carouFredSel({
-			items: {
-				width: 600,
-				height: 340
-			},
-			scroll:{
-				fx: "crossfade",
-				easing: "linear",
-				duration: 300
-			},
-			auto:{
-				timeoutDuration: 3000,
-				fx: "cover-fade",
-				easing: "swing",
-				pauseOnHover: true,
-			},			
-			pagination: "div.carousel-indicators"
-		});
-	});
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40176825-1', 'github.io');
+  ga('send', 'pageview');
